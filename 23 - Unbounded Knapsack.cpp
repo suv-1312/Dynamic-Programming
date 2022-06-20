653866034df86592b56b336a115d3c2bf9a6e063
suv-1312 <96910024+suv-1312@users.noreply.github.com>
2022-06-20 18:33:39 +0530

    Update 23 - Unbounded Knapsack.cpp

--- 23 - Unbounded Knapsack.cpp
+++ 23 - Unbounded Knapsack.cpp
@@ -1,5 +1,64 @@
 /*
+Problem Statement
+You are given ‘N’ items with certain ‘PROFIT’ and ‘WEIGHT’ and a knapsack with weight capacity ‘W’. You need to fill the knapsack with the items in such a way that you get the maximum profit. You are allowed to take one item multiple times.
+For Example
+Let us say we have 'N' = 3 items and a knapsack of capacity 'W' =  10
+'PROFIT' = { 5, 11, 13 }
+'WEIGHT' = { 2, 4, 6 }
 
+We can fill the knapsack as:
+
+1 item of weight 6 and 1 item of weight 4.
+1 item of weight 6 and 2 items of weight 2.
+2 items of weight 4 and 1 item of weight 2.
+5 items of weight 2.
+
+The maximum profit will be from case 3 i.e ‘27’. Therefore maximum profit = 27.
+Input Format
+The first line contains a single integer ‘T’ denoting the number of test cases.
+
+The first line of each test contains two integers ‘N’ - number of elements in the array and ‘W’ - Capacity of the knapsack.
+
+The second line of each test case contains profiti - profit of the item at the ‘i-th’ index.
+
+The third line of each test case contains weighti - weight of the item at the ‘i-th’ index
+Output Format
+For each test case, return an integer denoting the maximum profit that can be obtained by filling the knapsack.
+Note:
+You do not need to print anything; it has already been taken care of. Just implement the given function.
+Constraints
+1 <= T <= 50
+1 <= N <= 10^3
+1 <= W <= 10^3
+1 <= PROFIT[ i ] , WEIGHT[ i ] <= 10^8
+
+Time Limit: 1sec
+Sample Input 1
+2
+3 15
+7 2 4
+5 10 20
+2 3
+6 12
+4 17
+Sample Output 1
+21
+0
+Explanation Of Sample Input 1
+Test Case 1:  The given knapsack capacity is 15. We can fill the knapsack as [5, 5, 5] and [10, 5]. We will get a maximum profit of 21.
+
+Test Case 2: We can clearly see that no item has wait less than knapsack capacity. Therefore we can not fill knapsack with any item.
+Sample Input 2
+2
+4 7
+3 4 5 1
+4 1 2 3
+3 9
+1 2 3
+1 3 5
+Sample Output 2
+28
+9
 */
 
 //--------------------------------------------RECURSION------------------------------------------------
